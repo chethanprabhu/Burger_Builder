782f2746305cfa3d5815c665e956e7dabd023027
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-23 11:36:42 +0530

    small fix

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -6,28 +6,29 @@ const BurgerIngredient = (props) => {
 
     switch(props.type) {
         case "bread-bottom":
-            <div className={classes.BreadBottom}></div>
+            ingredient = <div className={classes.BreadBottom}></div>
             break;
         case "bread-top":
-            <div className={classes.BreadTop}>
+            ingredient = (<div className={classes.BreadTop}>
                 <div className={classes.Seeds1}></div>
                 <div className={classes.Seeds2}></div>
-            </div>
+            </div>)
             break;
         case "meat":
-            <div className={classes.Meat}></div>
+            ingredient = <div className={classes.Meat}></div>
             break;
         case "cheese":
-            <div className={classes.Cheese}></div>
+            ingredient = <div className={classes.Cheese}></div>
             break;
         case "salad":
-            <div className={classes.Salad}></div>
+            ingredient = <div className={classes.Salad}></div>
             break;
         case "bacon":
-            <div className={classes.Bacon}></div>
+            ingredient = <div className={classes.Bacon}></div>
             break;
         default: ingredient = null;
     }
+    return ingredient;
 }
 
 export default BurgerIngredient
