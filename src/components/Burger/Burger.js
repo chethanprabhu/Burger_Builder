00e9678a9e7e8a431cc23da27c266589334a8078
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-25 18:12:49 +0530

    handling if no ingredients wer added
    130

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,16 @@ import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient"
 import classes from "./Burger.module.css"
 
 const Burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map((ingredient) => {
+    let transformedIngredients = Object.keys(props.ingredients).map((ingredient) => {
         return [...Array(props.ingredients[ingredient])].map((_, index) => {
             return <BurgerIngredient key={ingredient + index} type={ingredient}></BurgerIngredient> 
         })
-    })
+    }).flat();
 
-    console.log(transformedIngredients);
-    
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
