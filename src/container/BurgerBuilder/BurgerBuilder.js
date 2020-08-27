814d0e8d4b6bed5f6c4c80c5da285d1ff98b30df
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-27 22:34:01 +0530

    Burger builder changes

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from  "react";
 import Burger from "../../components/Burger/Burger"
+import BurgerControls from "../../components/Burger/BurgerControls/BurgerControls"
 
 class BurgerBuilder extends Component {
 
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component {
         return (
             <React.Fragment>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger controller</div>
+                <BurgerControls/>
             </React.Fragment>
         )
     }
