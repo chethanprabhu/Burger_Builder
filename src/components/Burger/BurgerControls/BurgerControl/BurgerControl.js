d4595ebdcc0ef80aebb307da0bff2ad77684627f
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-09-08 15:08:27 +0530

    refactoring

--- src/components/Burger/BurgerControls/BurgerControl/BurgerControl.js
+++ src/components/Burger/BurgerControls/BurgerControl/BurgerControl.js
@@ -3,14 +3,10 @@ import classes from "./BurgerControl.module.css"
 
 const BurgerControl = (props) => {
 
-    let checkForDisabled = (state, type) => {
-        return state.ingredients[type] === 0;
-    }
-
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{props.label}</div>
-            <button disabled={checkForDisabled(props.state, props.type)} className={classes.Less} onClick={props.lessClicked}>-</button>
+            <button disabled={props.state.ingredients[props.type] === 0} className={classes.Less} onClick={props.lessClicked}>-</button>
             <button className={classes.More} onClick={props.moreClicked}>+</button>
         </div>
     )
