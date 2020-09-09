829d59c08a8207e487d918ec408c3bd0a5edc24f
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-09-09 17:15:29 +0530

    added logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../assets/images/burger-logo.png"
+import classes from "./Logo.module.css"
+
+const Logo = () => (
+    <div className={classes.Logo}>
+        <img src={logo}></img>
+    </div>
+)
+    
+
+
+export default Logo
