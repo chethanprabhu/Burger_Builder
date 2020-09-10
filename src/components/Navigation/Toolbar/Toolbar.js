67d7ae27f52a63e09a24fbffbdf524be7846a4c7
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-09-10 15:07:21 +0530

    replaced menu with hamburger icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react'
 import classes from "./Toolbar.module.css"
 import Logo from "../../Logo/Logo"
 import NavigationItems from "../NavigationItems/NavigationItems"
+import MenuLogo from "../../../assets/images/menu.png"
 
 const Toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div onClick={props.toggleClicked}>MENU</div>
+        <div onClick={props.toggleClicked}><img className={classes.Toggle} src={MenuLogo} alt="toggle"></img></div>
         <Logo height="80%"/>
         <nav className={classes.DesktopOnly}>
             <NavigationItems/>
